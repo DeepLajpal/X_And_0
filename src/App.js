f4db10d52782caac34808d4da61b9106aceaea55
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-02-04 00:06:53 +0530

    feat: add winner status display and calculate winner function in Tic-Tac-Toe game

--- src/App.js
+++ src/App.js
@@ -10,10 +10,16 @@ const Square = ({ value, onSquareClick }) => {
 export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
-
+  let winner = calculateWinner(squares);
+  let WINNER_STATUS = false;
+  if (winner === "X" || winner === "0") {
+    WINNER_STATUS = `Winner is: ${winner}`;
+  } else {
+    WINNER_STATUS = `Next Player: ${xIsNext ? "X" : "0"}`;
+  }
   const handleClick = (i) => {
     const nextSquares = squares.slice();
-    if (nextSquares[i]) {
+    if (nextSquares[i] || calculateWinner(squares)) {
       return;
     }
     if (xIsNext) {
@@ -24,8 +30,10 @@ export default function Board() {
     setXIsNext(!xIsNext);
     setSquares(nextSquares);
   };
+
   return (
     <>
+      <div className="status">{WINNER_STATUS ? WINNER_STATUS : null}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -44,3 +52,23 @@ export default function Board() {
     </>
   );
 }
+
+const calculateWinner = (squares) => {
+  const userWinningLinesCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < userWinningLinesCombinations.length; i++) {
+    const [a, b, c] = userWinningLinesCombinations[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
